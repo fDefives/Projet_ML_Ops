f3cfc8588402730e67bac947ebbfb0d568027685
Granier Sylvain <151046639+SylvainEODD@users.noreply.github.com>
2025-11-17 16:42:05 +0100

    image traitement

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MLOps',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='s.granier',
+    author_email='',
+    description=''
+)
